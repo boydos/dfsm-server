f1de9a6685d45ead08fb28c343ece42706bf32b4
byds520 <15910529587@163.com>
2018-05-03 23:48:36 +0800

    Init Document

---
+++ dfsm-mybatis/src/main/java/com/ds/dfsmmybatis/DfsmMybatisApplication.java
@@ -0,0 +1,12 @@
+package com.ds.dfsmmybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfsmMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DfsmMybatisApplication.class, args);
+    }
+}
