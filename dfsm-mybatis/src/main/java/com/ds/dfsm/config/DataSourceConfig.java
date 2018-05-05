d60152edff618d9ccef59d15ebe4f4fb2a68a7b2
byds <tongheizi@163.com>
2018-05-05 14:41:31 +0800

    modify sql

--- dfsm-mybatis/src/main/java/com/ds/dfsm/config/DataSourceConfig.java
+++ dfsm-mybatis/src/main/java/com/ds/dfsm/config/DataSourceConfig.java
@@ -1,10 +1,13 @@
 package com.ds.dfsm.config;
 
+import com.github.pagehelper.PageInterceptor;
+import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,12 +28,20 @@ public class DataSourceConfig {
     @Value("${mybatis.type-aliases-package}")
     protected String packageAliasLocation;
 
+    @Value("${mybatis.config-location}")
+    private String configLoacation;
+
+    @Autowired
+    protected PageInterceptor pageInterceptor;
+
     @Bean
     public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
         SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
         sqlSessionFactoryBean.setDataSource(dataSource);
+        sqlSessionFactoryBean.setPlugins(new Interceptor[]{pageInterceptor});
         sqlSessionFactoryBean.setTypeAliasesPackage(packageAliasLocation);
         sqlSessionFactoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources(mapperLoction));
+        sqlSessionFactoryBean.setConfigLocation(new PathMatchingResourcePatternResolver().getResource(configLoacation));
         return sqlSessionFactoryBean.getObject();
     }
 
--- dfsm-mybatis/src/main/java/com/ds/dfsm/pojo/User.java
+++ dfsm-mybatis/src/main/java/com/ds/dfsm/pojo/User.java
@@ -5,5 +5,4 @@ public class User {
     private String name;
     private String nickname;
     private Integer role;
-
 }
